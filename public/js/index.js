1ab54f7275b8a841f4d8e61665a5a58132084ac0
akashmaurya1998 <akashmaurya400>
2020-06-18 21:40:44 +0530

    After Adding meta tags and thinking its complete

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
+
+    $('#ABOUT').scrollTop(0);
+
     $('.nav-item').click(function() {
         $('.active').removeClass("active");
         $(this).addClass("active");
     });
+
+    $('nav .nav-link').click(function() {
+      event.preventDefault();
+        var hash = this.hash;
+        $('html,body').animate({
+            scrollTop: $(""+$(this).attr('href')).offset().top
+        }, 300);
+    });
 });
+
+if ($(window).width()<992) {
+  $('.para').removeClass("w-50");
+  $('span').removeClass('mr-4');
+  $('span').addClass('mr-2');
+  console.log("less");
+} else {
+  $('.para').addClass('w-50');
+  $('span').removeClass('mr-2');
+  $('span').addClass('mr-4');
+}
